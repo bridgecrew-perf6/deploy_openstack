ce629b81ba12e19fde43bde582b294eabe4fa2a7
rootly <48964029+rootly-be@users.noreply.github.com>
2021-12-27 17:07:53 +0100

    Update deploy_keystone_ubuntu.sh

--- deploy_keystone_ubuntu.sh
+++ deploy_keystone_ubuntu.sh
@@ -128,7 +128,7 @@ echo "### Installation keystone package"
 apt install keystone -y &> $LOGFILE
 
 echo "@@-> Edit the /etc/keystone/keystone.conf -> https://docs.openstack.org/keystone/wallaby/install/keystone-install-ubuntu.html"
-KEYSTONE_CON="mysql+pymysql:\/\/keystone:$KEYSTONE_DBPASS@$HOSTNAME\/keystone"
+KEYSTONE_CON="mysql+pymysql:\/\/keystone:${KEYSTONE_DBPASS}@${HOSTNAME}\/keystone"
 sed -i "s/connection = sqlite:\/\/\/\/var\/lib\/keystone\/keystone.db/connection = ${KEYSTONE_CON}/g" /etc/keystone/keystone.conf
 
 echo "### Configuring keystone database/table"
