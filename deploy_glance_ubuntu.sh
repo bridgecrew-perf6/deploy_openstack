8d8f15627b1169458b40025510ab85381bae1f6e
rootly <48964029+rootly-be@users.noreply.github.com>
2021-12-28 16:02:31 +0100

    Create deploy_glance_ubuntu.sh

---
+++ deploy_glance_ubuntu.sh
@@ -0,0 +1,52 @@
+clear
+OP_CONTROLLER_IP=10.0.0.11
+PASS_LEN=25
+
+LOGFILE=deploy_glance.log
+touch $LOGFILE
+
+GLANCE_DBPASS=$(openssl rand -hex $PASS_LEN) &> $LOGFILE
+echo $GLANCE_DBPASS > /root/glance_db_pass.txt
+
+mysql <<_EOF_
+  CREATE DATABASE keystone;
+  GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY '${GLANCE_DBPASS}';
+  GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY '${GLANCE_DBPASS}';
+_EOF_
+
+GLANCE_ADMINPASS=$(openssl rand -hex $PASS_LEN) &> $LOGFILE
+echo $GLANCE_ADMINPASS > /root/glance_admin_pass.txt
+
+openstack user create --domain default --password ${GLANCE_ADMINPASS}
+openstack role add --project service --user glance admin
+openstack service create --name glance --description "OpenStack Image" image
+openstack endpoint create --region RegionOne image public http://${HOSTNAME}:9292
+openstack endpoint create --region RegionOne image internal http://${HOSTNAME}:9292
+openstack endpoint create --region RegionOne image admin http://${HOSTNAME}:9292
+
+apt install -y glance
+
+GLANCE_CON="mysql+pymysql://glance:${GLANCE_DBPASS}@${HOSTNAME}/glance"
+
+crudini --set /etc/glance/glance-api.conf database connection $GLANCE_CON
+
+crudini --set /etc/glance/glance-api.conf keystone_authtoken www_authenticate_uri http://${HOSTNAME}:5000
+crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_url http://controller:5000
+crudini --set /etc/glance/glance-api.conf keystone_authtoken memcached_servers controller:11211
+crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_type password
+crudini --set /etc/glance/glance-api.conf keystone_authtoken project_domain_name Default
+crudini --set /etc/glance/glance-api.conf keystone_authtoken user_domain_name Default
+crudini --set /etc/glance/glance-api.conf keystone_authtoken project_name service
+crudini --set /etc/glance/glance-api.conf keystone_authtoken username glance
+crudini --set /etc/glance/glance-api.conf keystone_authtoken password $GLANCE_ADMINPASS
+
+crudini --set /etc/glance/glance-api.conf paste_deploy flavor keystone
+
+crudini --set /etc/glance/glance-api.conf glance_store stores file,http
+crudini --set /etc/glance/glance-api.conf glance_store default_store file
+crudini --set /etc/glance/glance-api.conf glance_store filesystem_store_datadir /var/lib/glance/images/
+
+su -s /bin/sh -c "glance-manage db_sync" glance
+
+systemctl restart glance-api
+
