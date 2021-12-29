041412d151906d9e0a87fb5094141a9643d43979
rootly <48964029+rootly-be@users.noreply.github.com>
2021-12-29 16:36:10 +0100

    Update deploy_keystone_ubuntu.sh

--- deploy_keystone_ubuntu.sh
+++ deploy_keystone_ubuntu.sh
@@ -141,7 +141,7 @@ ADMIN_PASS=$(openssl rand -hex $PASS_LEN) &> $LOGFILE
 
 keystone-manage bootstrap --bootstrap-password $ADMIN_PASS --bootstrap-admin-url http://$HOSTNAME:5000/v3/ --bootstrap-internal-url http://$HOSTNAME:5000/v3/ --bootstrap-public-url http://$HOSTNAME:5000/v3/ --bootstrap-region-id RegionOne &> $LOGFILE
 
-sed -i '1 i\ServerName ${HOSTNAME}' /etc/apache2/apache2.conf
+sed -i "1 i\ServerName $HOSTNAME" /etc/apache2/apache2.conf
 
 systemctl restart apache2.service &> $LOGFILE
 systemctl enable apache2.service &> $LOGFILE
